ebc5970a6e95b60f9af72cc1a812fefff18a910b
NikhilKapali <nikhilkapali12@gmail.com>
2021-02-25 17:21:37 +0545

    Landing Page 2

--- ecommerce-template-web-frontend/src/Home.js
+++ ecommerce-template-web-frontend/src/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CategoryNavbar from "./Navbar/CategoryNavbar";
 import Navbar from "./Navbar/Navbar";
+import LandingPage from "./homepage/LandingPage"
 function Home() {
   return (
     <>
       <Navbar />
       <CategoryNavbar />
+      <LandingPage />
     </>
   );
 }
