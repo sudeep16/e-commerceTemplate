f2e14dcff9d0bb418e10b47425ad68c0f2c42ef7
Shrawan-git <shrawanshaky3@gmail.com>
2021-02-25 13:30:04 +0545

    web front and back added

---
+++ ecommerce-template-web-frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CategoryNavbar from "./Navbar/CategoryNavbar";
+import Navbar from "./Navbar/Navbar";
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <CategoryNavbar />
+    </>
+  );
+}
+
+export default Home;
---
+++ ecommerce-template-web-frontend/src/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return <h1>This is navbar page</h1>;
+}
+
+export default Navbar;
