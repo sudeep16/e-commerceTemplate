70b21542a95ecb77d1f0d2446e4b9f56b6bac02c
Gunjan Shakya <gunjan.shakya3@gmail.com>
2021-02-25 16:14:55 +0545

    Login and registered (USER)

--- ecommerce-template-backend/index.js
+++ ecommerce-template-backend/index.js
@@ -4,11 +4,18 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
+//Routers
+const userRouter = require("./routes/users");
+
 const app = express();
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+//Routes
+app.use("/users", userRouter);
 
 //Database Config
 mongoose.connect(process.env.URL, {
