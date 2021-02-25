051469aad9c35be6c10333cd5849d0ba01e8f52d
sudeep16 <shakya_sudeep@hotmail.com>
2021-02-25 17:23:55 +0545

    Category NavBar

--- ecommerce-template-web-frontend/src/Home.js
+++ ecommerce-template-web-frontend/src/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CategoryNavbar from "./Navbar/CategoryNavbar";
+import LandingPage from "./homepage/LandingPage";
 import Navbar from "./Navbar/Navbar";
 function Home() {
   return (
     <>
       <Navbar />
       <CategoryNavbar />
+      <LandingPage />
     </>
   );
 }
--- ecommerce-template-web-frontend/src/Navbar/CategoryNavbar.js
+++ ecommerce-template-web-frontend/src/Navbar/CategoryNavbar.js
@@ -1,9 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function CategoryNavbar() {
-  return <h1>This is Category navbar page</h1>;
+  return (
+    <>
+      <CNavbar>
+        <CNavbarItem text="Electronics">
+          <DropDownMenu>
+            Radio
+            Television
+
+            Mobile
+          </DropDownMenu>
+
+        </CNavbarItem>
+        <CNavbarItem text="Accessories" >
+          <DropDownMenu />
+        </CNavbarItem>
+        <CNavbarItem text="Home & Living" />
+        <CNavbarItem text="Men Fashion" />
+        <CNavbarItem text="Women Fashion" />
+        <CNavbarItem text="Kids" />
+        <CNavbarItem text="Books" />
+      </CNavbar>
+    </>
+  );
 }
+
+function CNavbar(props) {
+  return (
+    <>
+      <nav className="category__navbar">
+        <ul className="navbar__catNav">{props.children}</ul>
+      </nav>
+    </>
+  );
+}
+
+function CNavbarItem(props) {
+  const [open, setOpen] = useState();
+
+  return (
+    <>
+      <li className="cnav__item">
+        <a href="#" className="cat__link" onClick={() => setOpen(!open)}>
+          {props.text}
+        </a>
+        {open && props.children}
+      </li>
+    </>
+  );
+}
+
+function DropDownMenu(props) {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        {props.children}
+        <span className="icon__right"> {props.rightIcon} </span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>Profile</DropdownItem>
+      <DropdownItem>Hello World</DropdownItem>
+    </div>
+  )
+}
+
+
+
 
 export default CategoryNavbar;
